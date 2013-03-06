17ab2b29f0c74fe05a47ed96b75d4f889ac450dc
Michael Meyer <michaelmeyer724@gmail.com>
2013-03-06 00:41:27 -0500

    Remove debug stuff, change a comment

--- src/main/java/com/mike724/worldpos/WPListener.java
+++ src/main/java/com/mike724/worldpos/WPListener.java
@@ -100,23 +100,10 @@ public class WPListener implements Listener {
 			for(Hostname hn : Settings.hostnames) {
 				String host = (hn.getHostname().split(":").length==2) ? hn.getHostname() : hn.getHostname()+":"+plugin.getServer().getPort();
 				
-				//Remove oddity with SRV records
+				//Remove oddity in some hostnames
 				String hnClient = event.getHostname().replaceFirst(".:", ":");
 				
-				//DEBUG
-				plugin.getLogger().info("IN LOOP");
-				plugin.getLogger().info("Hostname from config: "+hn.getHostname());
-				plugin.getLogger().info("Modified hostname from config (appends server port): "+host);
-				plugin.getLogger().info("getHostname method returned: "+hnClient);
-				
 				if(hnClient.equalsIgnoreCase(host)) {
-					
-					//DEBUG
-					plugin.getLogger().info("HOSTNAMES DID EQUAL EACH OTHER");
-					plugin.getLogger().info("Hostname from config: "+hn.getHostname());
-					plugin.getLogger().info("Modified hostname from config (appends server port): "+host);
-					plugin.getLogger().info("getHostname method returned: "+hnClient);
-					
 					Player p = event.getPlayer();
 					if(!p.hasPermission("WorldPos.hostname."+hn.getKey())) {
 						event.setResult(PlayerLoginEvent.Result.KICK_OTHER);
